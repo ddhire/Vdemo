f75de30085bbd7b772b10c22429c823a78240493
dnya <dd@gmail.com>
2022-07-20 08:53:16 +0000

    This is Demo

---
+++ src/test/java/com/Vdemo/Vdemo/VdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vdemo.Vdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
