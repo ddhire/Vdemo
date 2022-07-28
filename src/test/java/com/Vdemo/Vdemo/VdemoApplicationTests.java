20c77c5e237c2df4519d12b151bcfa7ca0acae40
ddhire <59061282+ddhire@users.noreply.github.com>
2022-07-28 16:44:32 +0530

    Update VdemoApplicationTests.java

--- src/test/java/com/Vdemo/Vdemo/VdemoApplicationTests.java
+++ src/test/java/com/Vdemo/Vdemo/VdemoApplicationTests.java
@@ -1,10 +1,17 @@
 package com.Vdemo.Vdemo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VdemoApplicationTests {
+	private ReviewAnalyserApplication analyser = new ReviewAnalyserApplication();
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
+
 
 	@Test
 	void contextLoads() {
