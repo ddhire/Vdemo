9584cb9d36611e244fbb367342ac2ecc079348ac
ddhire <59061282+ddhire@users.noreply.github.com>
2022-07-28 18:08:53 +0530

    Update VdemoApplicationTests.java

--- src/test/java/com/Vdemo/Vdemo/VdemoApplicationTests.java
+++ src/test/java/com/Vdemo/Vdemo/VdemoApplicationTests.java
@@ -9,12 +9,9 @@ class VdemoApplicationTests {
 	private VdemoApplication analyser = new VdemoApplication();
     @Test	 
     public void testWordCount() {     	 
-   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+   	 assertEquals(1,analyser.getWordCount("T"));	 
     }
 
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
